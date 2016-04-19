470266eb96f89026c13c73a44c952e46e5120a52
Abraham Sánchez <abraham@twintags.com>
2016-04-19 13:16:52 +0200

    Disable JTA auto configuration
    This ensures transaction management is managed by
    GORM when deploying to a Wildfly app server.

--- grails-app/init/grails3/library/example/Application.groovy
+++ grails-app/init/grails3/library/example/Application.groovy
@@ -1,8 +1,11 @@
 package grails3.library.example
 
+import org.springframework.boot.autoconfigure.transaction.jta.*
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
+@EnableAutoConfiguration(exclude = [JtaAutoConfiguration])
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
